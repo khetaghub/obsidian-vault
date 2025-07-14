1b466d95fe837df7d52bf5d0823b4239ba68984e
Hetag <h.kastuev99@inbox.ru>
2025-07-14 18:22:04 +0300

    Update: 14.07.2025 18:22:04,57

--- .scripts/push.bat
+++ .scripts/push.bat
@@ -1,12 +1,12 @@
 @echo off
-chcp 65001
+chcp 65001 >nul
 setlocal
 
 set REPO_DIR=../.
 
 :: Форматируем дату и время без десятых секунды
 for /f "tokens=1-3 delims=." %%a in ("%time%") do set CLEAN_TIME=%%a
-set COMMIT_MSG="Update: %date% %CLEAN_TIME%
+set COMMIT_MSG="Update: %date% %CLEAN_TIME%"
 
 :: Проверка Git
 where git >nul 2>&1
@@ -17,23 +17,24 @@ if %errorlevel% neq 0 (
 )
 
 if not exist "%REPO_DIR%" (
-    echo Ошибка: Папка "%REPO_DIR%" не найдена. Сначала запустите clone_repo.bat.
+    echo Ошибка: Папка "%REPO_DIR%" не найдена.
     pause
     exit /b 1
 )
 
 cd %REPO_DIR%
+
 echo Добавляем все изменения...
 git add --all
 
 echo Коммитим: %COMMIT_MSG%
-git commit -m %COMMIT_MSG% --quiet
+git commit -m %COMMIT_MSG% --quiet 2>nul
 if %errorlevel% neq 0 (
-    echo Нет изменений для коммита или ошибка.
-    pause
-    exit /b 1
+    echo Нет изменений для коммита. Пропускаем.
+    goto PUSH
 )
 
+:PUSH
 echo Пушим...
 git push --quiet
 if %errorlevel% neq 0 (
